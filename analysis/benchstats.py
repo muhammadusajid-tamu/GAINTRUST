97c98d4ca6f914e5fcfa4fc2e50bb921f56f41d9
daaangdennis <dennisdang80@yahoo.com>
2025-04-23 13:35:54 -0500

    Deleted test.sh, testdriver.py, and analysis folder

--- analysis/benchstats.py
+++
@@ -1,7 +0,0 @@
-from analysis.config import feedback_strats, llms, projects, bm_data, data, lints
-
-for p in projects:
-    b = bm_data[bm_data["project"] == p].sort_values("language")
-    loc = " \\textbf{/} ".join([str(int(i)) for i in [b['loc'].min(), b['loc'].max(), b['loc'].mean()]])
-    funcs = " \\textbf{/} ".join([str(round(i,1)) for i in [b['funcs'].min(), b['funcs'].max(), b['funcs'].mean()]])
-    print(f"\\textit{{{p}}} & {b['language'].iloc[0].capitalize()} & {len(b)} & {loc} & {funcs} \\\\")
